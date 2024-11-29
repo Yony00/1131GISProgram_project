61cb0b834c4dfb97cd64f0a46513280029a5eb08
RGT1143022 <s1143022@gm.ncue.edu.tw>
2024-11-30 01:51:47 +0800

    Update 3_📍_Marker_Cluster.py

--- pages/3_📍_Marker_Cluster.py
+++ pages/3_📍_Marker_Cluster.py
@@ -21,16 +21,19 @@ with st.expander("See source code"):
         m = leafmap.Map(center=[40, -100], zoom=4)
         cities = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
         regions = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson"
+        MS="https://github.com/RGT1143022/datafor1127/releases/download/v1.0.0/MSwithGEO.geojson"
 
-        m.add_geojson(regions, layer_name="US Regions")
-        m.add_points_from_xy(
-            cities,
-            x="longitude",
-            y="latitude",
-            color_column="region",
-            icon_names=["gear", "map", "leaf", "globe"],
-            spin=True,
-            add_legend=True,
-        )
+       # m.add_geojson(regions, layer_name="US Regions")
+        m.add_geojson(MS, layer_name="Man Single Player Count")
+
+        #m.add_points_from_xy(
+        #    cities,
+        #    x="longitude",
+        #    y="latitude",
+        #    color_column="region",
+        #    icon_names=["gear", "map", "leaf", "globe"],
+        #    spin=True,
+        #    add_legend=True,
+        #)
 
 m.to_streamlit(height=700)
