533d0bc1bb339e28f29e2132bc6f6e68da53916a
RGT1143022 <s1143022@gm.ncue.edu.tw>
2024-12-01 00:22:33 +0800

    Update BWF Men's Singles World Ranking100.py

--- pages/BWF Men's Singles World Ranking100.py
+++ pages/BWF Men's Singles World Ranking100.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from scrape_bwf_ranking import scrape_bwf_ranking  # 引入爬蟲腳本
+from scrape_bwf_ranking import scrape_bwf_ranking_initial, scrape_bwf_ranking_by_date
 
 # 設定頁面標題
 st.title("BWF Men's Singles World Ranking")
@@ -8,12 +8,11 @@ st.title("BWF Men's Singles World Ranking")
 # 用來儲存日期和ID對應字典
 date_id_dict = {}
 
-# 當按下按鈕時，抓取資料並顯示
+# 第一個按鈕：抓取固定日期11/26/2024資料並取得ID對應字典
 if st.button("Get Ranking for 11/26/2024"):
     try:
-        # 設定固定的 URL 並執行爬蟲
-        url = "https://bwf.tournamentsoftware.com/ranking/category.aspx?id=43340&category=472&C472FOC=&p=1&ps=100"
-        df, date_id_dict = scrape_bwf_ranking(url)
+        # 呼叫第一個爬蟲，獲取排名資料並取得日期- ID字典
+        df, date_id_dict = scrape_bwf_ranking_initial()
 
         # 顯示排名資料
         st.write("Below is the BWF Men's Singles World Ranking for 11/26/2024:", df)
@@ -23,13 +22,10 @@ if st.button("Get Ranking for 11/26/2024"):
         date_options = list(date_id_dict.keys())
         selected_date = st.selectbox("Select Date", date_options)
 
-        # 當使用者選擇日期後，再次執行爬蟲並顯示選擇日期的資料
+        # 第二個按鈕：根據選擇的日期執行爬蟲
         if selected_date:
             selected_id = date_id_dict[selected_date]
-            selected_url = f"https://bwf.tournamentsoftware.com/ranking/category.aspx?id={selected_id}&category=472&C472FOC=&p=1&ps=100"
-
-            # 執行爬蟲抓取所選日期的資料
-            df_selected, _ = scrape_bwf_ranking(selected_url)
+            df_selected = scrape_bwf_ranking_by_date(selected_id)
 
             # 顯示選擇日期的排名資料
             st.write(f"Below is the BWF Men's Singles World Ranking for {selected_date}:", df_selected)
@@ -44,5 +40,6 @@ if st.button("Get Ranking for 11/26/2024"):
 
     except Exception as e:
         st.error(f"Error occurred: {e}")
+
 
 
