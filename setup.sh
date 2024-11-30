379bdac3d0f07f27e4925222abda5a2b5f5d50a2
RGT1143022 <s1143022@gm.ncue.edu.tw>
2024-11-30 22:36:32 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,3 @@
 # 安裝 Python 依賴
 echo "Installing Python dependencies..."
 pip install -r requirements.txt
-
-# 安裝 Playwright 和瀏覽器
-echo "Installing Playwright browsers..."
-playwright install --with-deps
-
-# 顯示安裝後的狀況
-echo "Playwright browsers installed successfully."
