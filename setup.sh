caef7d2adbbc9f28bd454aa28046ff206b244c18
RGT1143022 <s1143022@gm.ncue.edu.tw>
2024-11-30 22:13:32 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# 確保 pip 是最新版本
+echo "Upgrading pip..."
+pip install --upgrade pip
+
 # 安裝 Python 依賴
 echo "Installing Python dependencies..."
 pip install -r requirements.txt
@@ -7,3 +11,7 @@ pip install -r requirements.txt
 # 安裝 Playwright 瀏覽器
 echo "Installing Playwright browsers..."
 playwright install
+
+# 確保 Playwright 安裝完成，測試是否可以成功啟動
+echo "Verifying Playwright installation..."
+playwright --version
