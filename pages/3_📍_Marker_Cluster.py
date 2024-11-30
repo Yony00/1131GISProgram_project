cf1ef8ea729c6b5cb3787708881e6be45922d0ed
RGT1143022 <s1143022@gm.ncue.edu.tw>
2024-11-30 17:48:31 +0800

    Update 3_📍_Marker_Cluster.py

--- pages/3_📍_Marker_Cluster.py
+++ pages/3_📍_Marker_Cluster.py
@@ -48,5 +48,4 @@ def load_data():
 
 
 data_frame = load_data()
-st.write("1 + 1 = ", 2)
-st.write("Below is a DataFrame:", data_frame, "Above is a dataframe.")
+st.write("Below is a DataFrame:", data_frame, )
