f7219b08e1486df960e7699208019cc13cbf2bab
RGT1143022 <s1143022@gm.ncue.edu.tw>
2024-12-05 12:42:23 +0800

    Update BWF Men's Singles World Ranking100.py

--- pages/BWF Men's Singles World Ranking100.py
+++ pages/BWF Men's Singles World Ranking100.py
@@ -10,19 +10,10 @@ import matplotlib.pyplot as plt          # 匯入pyplot模組
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from IPython.core.display import display
-# Colab 進行matplotlib繪圖時顯示繁體中文
-# 下載台北思源黑體並命名taipei_sans_tc_beta.ttf，移至指定路徑
-!wget -O TaipeiSansTCBeta-Regular.ttf https://drive.google.com/uc?id=1eGAsTN1HBpJAkeVM57_C7ccp7hbgSz3_&export=download
+
 
 import matplotlib
 
-# 改style要在改font之前
-# plt.style.use('seaborn')
-
-matplotlib.font_manager.fontManager.addfont('TaipeiSansTCBeta-Regular.ttf')
-matplotlib.rc('font', family='Taipei Sans TC Beta')
-
-!pip install squarify
 
 import seaborn as sns
 import plotly.offline
