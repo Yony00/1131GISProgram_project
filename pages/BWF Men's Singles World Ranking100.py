63ea674ce570723e0160509cd30ae2850d365343
RGT1143022 <s1143022@gm.ncue.edu.tw>
2024-12-05 12:14:51 +0800

    Update BWF Men's Singles World Ranking100.py

--- pages/BWF Men's Singles World Ranking100.py
+++ pages/BWF Men's Singles World Ranking100.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import geopandas as gpd
 from scrape_bwf_ranking import scrape_bwf_ranking  # 引入第一次爬蟲的函數
 from scrape_bwf_ranking_by_date import scrape_bwf_ranking_by_date  # 引入第二次爬蟲的函數
 
@@ -25,6 +26,7 @@ table_area = st.container()
 
 # 表格的左右分區
 row1_1, row1_2 = table_area.columns((1, 1))
+row2_1, row2_2 = table_area.columns((1, 1))
 
 # 檢查是否已經存儲過第一次爬蟲的資料
 if "df_initial" not in st.session_state:  # 只有在第一次爬蟲未完成時才會執行
@@ -32,28 +34,52 @@ if "df_initial" not in st.session_state:  # 只有在第一次爬蟲未完成時
         url = "https://bwf.tournamentsoftware.com/ranking/category.aspx?id=43340&category=472&C472FOC=&p=1&ps=100"
 
         # 呼叫第一次爬蟲，獲取排名資料並抓取日期-ID對應字典
-        df_initial, date_id_dict = scrape_bwf_ranking(url)
+        df_initial, date_id_dict,new_date = scrape_bwf_ranking(url)
 
         # 儲存第一次爬蟲結果到 session_state 中
         df_initial.set_index("Rank", inplace=True)
         st.session_state.df_initial = df_initial
         st.session_state.date_id_dict = date_id_dict  # 儲存日期-ID對應字典
         st.session_state.first_scrape_done = True  # 設定標記，表示第一次爬蟲已經完成
+        st.session_state.new_date=new_date # 儲存最新日期
     except Exception as e:
         st.error(f"Error occurred: {e}")
 
-# 顯示 11/26/2024 的排名資料
-if "df_initial" in st.session_state:
-    with row1_1:
-        st.write("Below is the BWF Men's Singles World Ranking for 11/26/2024:")
-        st.write(st.session_state.df_initial)
 
 # 如果已經成功取得日期-ID 對應字典，生成 selectbox
 if "date_id_dict" in st.session_state:
     date_id_dict = st.session_state.date_id_dict
 
+##################
+        # 使用 selectbox 讓使用者選擇日期
+    with row2_1:
+        options = list(date_id_dict.keys())
+        index = options.index(st.session_state.new_date)
+        selected_date1 = st.selectbox("選擇欲查詢的日期", [""] +options,index)
+
+    # 如果選擇了日期
+    if selected_date1:
+        try:
+            # 根據選擇的日期，獲取對應的 ID
+            selected_id1 = date_id_dict[selected_date1]
+
+            # 呼叫第二次爬蟲，抓取該日期的排名資料
+            df_selected1 = scrape_bwf_ranking_by_date(selected_id1)
+            df_selected1.set_index("Rank", inplace=True)
+
+            # 顯示選擇日期的排名資料於 row1_2
+            with row1_2:
+                st.write(f"Below is the BWF Men's Singles World Ranking for {selected_date1}:")
+                st.write(df_selected1)
+        except Exception as e:
+            st.error(f"Error occurred while fetching data for {selected_date1}: {e}")
+
+#################
+
+    
     # 使用 selectbox 讓使用者選擇日期
-    selected_date = st.selectbox("Select a date to view the rankings:", [""] + list(date_id_dict.keys()))
+    with row2_2:
+        selected_date = st.selectbox("選擇欲查詢的日期", [""] + list(date_id_dict.keys()))
 
     # 如果選擇了日期
     if selected_date:
@@ -71,3 +97,14 @@ if "date_id_dict" in st.session_state:
                 st.write(df_selected)
         except Exception as e:
             st.error(f"Error occurred while fetching data for {selected_date}: {e}")
+
+#world_country=gpd.read_file("https://github.com/RGT1143022/BWF_world_country/releases/download/v1.0.0/BWF_world_country_true.geojson")
+
+#案國家分組-最新日期
+#GB_country=st.session_state.df_initial.groupby(by=['Country']).agg(
+#    player_count=('player_name', len),
+#    playername=('player_name',';'.join)
+#    )
+#all_event_G_country_TOP10=all_event_G_country.nlargest(10,"player_count")
+#fig, axes = plt.subplots(figsize=(8,8 ))
+#sns.barplot(x="player_count", y="Country", data=all_event_G_country_TOP10)
