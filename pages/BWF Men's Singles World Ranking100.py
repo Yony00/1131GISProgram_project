d505410e5ea5505000c8a2f391aeac02d76fc540
RGT1143022 <s1143022@gm.ncue.edu.tw>
2024-12-06 23:33:20 +0800

    Update BWF Men's Singles World Ranking100.py

--- pages/BWF Men's Singles World Ranking100.py
+++ pages/BWF Men's Singles World Ranking100.py
@@ -281,7 +281,7 @@ if selected_date2:
 
     
     # 在左側地圖添加第一個 GeoDataFrame
-    m = leafmap.Map(center=(0, 0), zoom=2)
+    m = leafmap.Map(center=(0, 0), zoom=2,split_map=True)
     m.split_map(left_layer="Layer 1")
     m.add_gdf(
         gdf1,
