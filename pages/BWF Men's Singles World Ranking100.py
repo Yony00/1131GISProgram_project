ff43752c3a6e7e363937c509e6c5f1f0f38ae9cf
RGT1143022 <s1143022@gm.ncue.edu.tw>
2024-12-07 15:40:37 +0800

    Update BWF Men's Singles World Ranking100.py

--- pages/BWF Men's Singles World Ranking100.py
+++ pages/BWF Men's Singles World Ranking100.py
@@ -274,7 +274,7 @@ if selected_date2:
 
          #顯示地圖
         def sync_view(e):
-            bounds = 1.get_bounds()
+            bounds = m.get_bounds()
             m2.fit_bounds(bounds)
         m2.on_move(sync_view)
         m2.to_streamlit()
