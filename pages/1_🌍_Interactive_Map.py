c1e623db188426006bdca2489442c0ac248c1bd8
RGT1143022 <s1143022@gm.ncue.edu.tw>
2024-12-12 00:26:12 +0800

    Update 1_🌍_Interactive_Map.py

--- pages/1_🌍_Interactive_Map.py
+++ pages/1_🌍_Interactive_Map.py
@@ -89,7 +89,7 @@ initial_location = [40, -100]
 initial_zoom = 4
 
 # 建立第一個 Folium 地圖
-m1 = folium.Map(location=initial_location, zoom_start=initial_zoom)
+m1 = leafmap.Map(location=initial_location, zoom_start=initial_zoom)
 output1 = st_folium(m1, height=500, key="map1")
 
 # 取得第一個地圖的狀態
@@ -101,7 +101,7 @@ if output1:
 st.write(center)
 
 # 建立第二個地圖，將第一個地圖的狀態應用到第二個地圖
-m2 = folium.Map(location=center, zoom_start=zoom,tiles="Cartodb dark_matter")
+m2 = leafmap.Map(location=center, zoom_start=zoom,tiles="Cartodb dark_matter")
 output2 = st_folium(m2, height=500, key="map2")
 
 st.write("地圖1中心:", output1.get("center", initial_location))
