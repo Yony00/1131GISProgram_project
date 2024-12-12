ae71af0b680a58d684e22a12765003c977c060dc
RGT1143022 <s1143022@gm.ncue.edu.tw>
2024-12-12 13:26:51 +0800

    Update Split_Map.py

--- pages/Split_Map.py
+++ pages/Split_Map.py
@@ -1,12 +1,20 @@
+
+
 import streamlit as st
-import leafmap.foliumap as leafmap
 
-options = list(("男子單打","男子雙打","女子單打","女子雙打","混合單打"))
-index = options[1]
+# 可選項目列表
+options = ["男子單打", "男子雙打", "女子單打", "女子雙打", "混合雙打"]
 
-selected_date1 = st.selectbox(
-    "選擇欲查詢的日期 (預設最新日期)",
-    options,
-    index=index,
-    key="selectbox_date1",  # 添加唯一的 key
+# 預設選中第二項 "男子雙打"
+index = 1  # 索引從 0 開始
+
+# 顯示下拉選單
+selected_event = st.selectbox(
+    "選擇欲查詢的項目",  # 顯示的標題
+    options,  # 選項列表
+    index=index,  # 預設選中的索引
+    key="selectbox_event",  # 唯一的 key
 )
+
+# 顯示選中的項目
+st.write(f"你選擇了: {selected_event}")
