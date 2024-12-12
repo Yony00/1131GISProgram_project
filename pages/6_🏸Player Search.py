1800e9fdae69078605f4032461ca6f593984f766
RGT1143022 <s1143022@gm.ncue.edu.tw>
2024-12-12 13:34:34 +0800

    Update 6_🏸Player Search.py

--- pages/6_🏸Player Search.py
+++ pages/6_🏸Player Search.py
@@ -42,6 +42,16 @@ with row0_2:
         index=index,  # 預設選中的索引
         key="selectbox_event",  # 唯一的 key
     )
+    # 顯示文字輸入框
+    player_name = st.text_input("請輸入欲查詢的選手名(雙打則以/區隔)：", "", key="player_name")
+    # 顯示 "確定輸入" 按鈕
+    if st.button("確定輸入"):
+    # 當按鈕被點擊後顯示用戶輸入的內容
+        if user_input.strip():  # 檢查輸入是否非空
+            st.write(f"你輸入的內容是：{user_input}")
+        else:
+        st.warning("請輸入有效文字！")
+
 
 # 用來顯示表格的區域
 table_area = st.container()
