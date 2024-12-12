0dee05528d2abbe3197b66ad8c84e2eb2ca5710b
RGT1143022 <s1143022@gm.ncue.edu.tw>
2024-12-13 00:53:46 +0800

    Update 6_🏸Player Search.py

--- pages/6_🏸Player Search.py
+++ pages/6_🏸Player Search.py
@@ -187,6 +187,9 @@ if st.session_state.df is not None:
             # 繪製折線圖，僅顯示年份
             fig, ax = plt.subplots(figsize=(10, 6))
             sns.lineplot(data=plt_df, x=plt_df['Date'].dt.year, y='Points', ax=ax)
+            # 繪製 Y 軸的連續變數刻度
+            y_ticks = range(20000, plt_df['Points'].max() + 10000, 10000)  # 自動生成連續刻度
+            plt.yticks(y_ticks)
             ax.set_xlabel('Year')
             ax.set_ylabel('Points')
             ax.set_title('積分、排名變化')
