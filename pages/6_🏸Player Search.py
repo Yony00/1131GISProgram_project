537b2c3156926f01de1321f5bbed713e4ed3dd2e
RGT1143022 <s1143022@gm.ncue.edu.tw>
2024-12-12 23:05:24 +0800

    Update 6_🏸Player Search.py

--- pages/6_🏸Player Search.py
+++ pages/6_🏸Player Search.py
@@ -135,7 +135,7 @@ if selected_date1:
 #     player_name = st.text_input("請輸入欲查詢的選手名(組合名)，格式參考上表：", "", key="player_name")
 # st.markdown(f"<h4>以下是關於 {search_event} 項目， {player_name} 選手的歷年排名變化</h2>", unsafe_allow_html=True)
 
-# row2_1, row2_2, row2_3 = st.columns((1,1,1))
+row2_1, row2_2, row2_3 = st.columns((1,1,1))
 
 
 
