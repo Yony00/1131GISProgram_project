bd294d6715819a44305f63e560d139f0afc9c4c9
RGT1143022 <s1143022@gm.ncue.edu.tw>
2024-12-13 21:51:04 +0800

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -4,12 +4,12 @@ import geopandas as gpd
 from streamlit_folium import st_folium
 
 # 創建第一個地圖 m1
-m1 = folium.Map(location=[120, 24], zoom_start=5)  # 設定初始中心點和縮放級別
-folium.Marker([120, 25], popup='Point 1').add_to(m1)  # 在地圖上添加點物件
+m1 = folium.Map(location=[0, 0], zoom_start=1)  # 設定初始中心點和縮放級別
+folium.Marker([25, 120], popup='Point 1').add_to(m1)  # 在地圖上添加點物件
 
 # 創建第二個地圖 m2
-m2 = folium.Map(location=[120, 24], zoom_start=5)  # 設定初始中心點和縮放級別
-folium.Marker([120, 24], popup='Point 2').add_to(m2)  # 在地圖上添加點物件
+m2 = folium.Map(location=[0, 0], zoom_start=1)  # 設定初始中心點和縮放級別
+folium.Marker([24, 120], popup='Point 2').add_to(m2)  # 在地圖上添加點物件
 
 # 使用 st_folium 在 Streamlit 中顯示地圖
 import streamlit as st
