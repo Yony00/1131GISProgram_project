f6c6771823394eefc18381ee35954e386dda6622
RGT1143022 <s1143022@gm.ncue.edu.tw>
2024-12-14 00:08:23 +0800

    Update 6_🏸Player Search.py

--- pages/6_🏸Player Search.py
+++ pages/6_🏸Player Search.py
@@ -226,12 +226,13 @@ if st.session_state.df is not None:
             
             # 添加一條紅色水平線在 y=0 處
             ax1.axhline(y=0, color='red', linestyle='--')
-            ax1.axhline(y=105, color='brown', linestyle='--')
             # 右Y軸（Rank）折線圖
             ax2 = ax1.twinx()  # 共享 x 軸
             sns.lineplot(data=plt_df, x=plt_df['Date'].dt.year, y='Rank', ax=ax2, color='green')
             y_ticks2 = range(0, plt_df['Rank'].max() + 10, 10)  # 自動生成連續刻度
             ax2.set_ylabel('Rank', color='green')
+            ax2.axhline(y=105, color='brown', linestyle='--')
+
             ax2.set_yticks(y_ticks2)
             
             # 繪製 Y 軸的連續變數刻度
@@ -240,9 +241,9 @@ if st.session_state.df is not None:
             
             # 使用線作為 handles 並顯示圖例
             red_line = ax1.axhline(y=0, color='red', linestyle='--')
-            brown_line = ax1.axhline(y=0, color='brown', linestyle='--')
+            brown_line = ax2.axhline(y=0, color='brown', linestyle='--')
             ax1.legend(handles=[red_line], labels=['Points nodata'], fontsize=20, loc='upper left')
-            ax1.legend(handles=[brown_line], labels=['Rank nodata'], fontsize=20, loc='upper left')
+            ax2.legend(handles=[brown_line], labels=['Rank nodata'], fontsize=20, loc='upper left')
             st.pyplot(fig)
             
         if user_choice == "右表":
