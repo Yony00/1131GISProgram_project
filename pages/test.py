1c68d0e749e3a549f58487dc92b6186af1dabb25
RGT1143022 <s1143022@gm.ncue.edu.tw>
2024-12-13 21:53:58 +0800

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -2,18 +2,19 @@ import folium
 import pandas as pd
 import geopandas as gpd
 from streamlit_folium import st_folium
-
-# 創建第一個地圖 m1
-m1 = folium.Map(location=[0, 0], zoom_start=1)  # 設定初始中心點和縮放級別
-folium.Marker([25, 120], popup='Point 1').add_to(m1)  # 在地圖上添加點物件
-
-# 創建第二個地圖 m2
-m2 = folium.Map(location=[0, 0], zoom_start=1)  # 設定初始中心點和縮放級別
-folium.Marker([24, 120], popup='Point 2').add_to(m2)  # 在地圖上添加點物件
-
-# 使用 st_folium 在 Streamlit 中顯示地圖
 import streamlit as st
 
+# 創建第一個地圖 m1
+m1 = folium.Map(location=[0, 0], zoom_start=5)  # 設定初始中心點和縮放級別
+folium.Marker([24, 120], popup='Point 1').add_to(m1)  # 在地圖上添加點物件
+
+# 創建第二個地圖 m2
+m2 = folium.Map(location=[0, 0], zoom_start=5)  # 設定初始中心點和縮放級別
+
+# 添加 gdf2 到地圖
+folium.Marker([25, 120], popup='Point 2').add_to(m2)  # 在地圖上添加點物件
+
+# 使用 st_folium 在 Streamlit 中顯示地圖
 row1, row2 = st.columns(2)
 
 with row1:
@@ -21,3 +22,11 @@ with row1:
 
 with row2:
     output2 = st_folium(m2, width=400, height=300, key="map2")
+
+    # 手動輸入中心位置
+    lat = st.number_input('輸入緯度', min_value=-90.0, max_value=90.0, value=output2["center"]["lat"], format="%.6f")
+    lon = st.number_input('輸入經度', min_value=-180.0, max_value=180.0, value=output2["center"]["lng"], format="%.6f")
+
+    # 更新地圖的中心
+    m2.location = [lat, lon]
+    st_folium(m2, width=400, height=300, key="map2")  # 重新顯示地圖
