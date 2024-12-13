f90964baeab4e5bb4185e80a350fec6ae01ec821
RGT1143022 <s1143022@gm.ncue.edu.tw>
2024-12-13 16:22:41 +0800

    Update 6_🏸Player Search.py

--- pages/6_🏸Player Search.py
+++ pages/6_🏸Player Search.py
@@ -419,14 +419,13 @@ if st.session_state.df is not None:
         CP_df_event_MXD= MXD_df[MXD_df['Country'] == country]
 
         # 項目名稱列表
-        events = ["Men's Singles", "Women's Singles", "Men's Doubles", "Women's Doubles", "Mixed Doubles"]
-        
+        events =["男子單打", "男子雙打", "女子單打", "女子雙打", "混合雙打"]
         # 建立圖表
         fig = go.Figure()
         
         # 添加每個項目的長條圖
         fig.add_trace(go.Bar(
-            name="Men's Singles",
+            name="男子單打",
             x=CP_df_event_MS['Player'],  # X 軸：選手名字
             y=CP_df_event_MS['Points']    ,    # Y 軸：積分
             text=CP_df_event_MS['Rank'],      # 在長條圖上顯示排名
@@ -435,7 +434,7 @@ if st.session_state.df is not None:
         ))
         
         fig.add_trace(go.Bar(
-            name="Women's Singles",
+            name="女子單打",
             x=CP_df_event_WS['Player'], 
             y=CP_df_event_WS['Points'],
             text=CP_df_event_WS['Rank'],      # 在長條圖上顯示排名
@@ -444,7 +443,7 @@ if st.session_state.df is not None:
         ))
         
         fig.add_trace(go.Bar(
-            name="Men's Doubles",
+            name="男子雙打",
             x=CP_df_event_MD['Player'], 
             y=CP_df_event_MD['Points'],
             text=CP_df_event_MD['Rank'],      # 在長條圖上顯示排名
@@ -453,7 +452,7 @@ if st.session_state.df is not None:
         ))
         
         fig.add_trace(go.Bar(
-            name="Women's Doubles",
+            name="女子雙打",
             x=CP_df_event_WD['Player'], 
             y=CP_df_event_WD['Points'],
             text=CP_df_event_WD['Rank'],      # 在長條圖上顯示排名
@@ -462,10 +461,19 @@ if st.session_state.df is not None:
         ))
         
         fig.add_trace(go.Bar(
-            name="Mixed Doubles",
+            name="混合雙打",
             x=CP_df_event_MXD['Player'], 
             y=CP_df_event_MXD['Points'],
             text=CP_df_event_MXD['Rank'],      # 在長條圖上顯示排名
+            textposition='inside',  # 設置文字顯示位置
+            textfont=dict(size=18),
+        ))
+        #選擇的選手
+        fig.add_trace(go.Bar(
+            name=selected_event,
+            x=highlight_player, 
+            y=CP_df_event.loc[CP_df_event['Player'] == highlight_player, 'Points'].values,
+            text=CP_df_event.loc[CP_df_event['Player'] == highlight_player, 'Rank'].values,      # 在長條圖上顯示排名
             textposition='inside',  # 設置文字顯示位置
             textfont=dict(size=18),
         ))
@@ -473,7 +481,7 @@ if st.session_state.df is not None:
         # 更新圖表標題與布局
         fig.update_layout(
             barmode='group',  # 以群組形式顯示柱狀圖
-            title=f"該時期 {country} 不同項目中，不同選手的積分、排名 \n 圖中數字為世界排名(Rank)",
+            title=f"該時期 {country} 不同項目中，不同選手的積分、排名，圖中數字為世界排名(Rank)",
             xaxis_title="Players",
             yaxis=dict(
                         title='Points',          # Y 軸的標題
