0aeac6f82dd70c556c4751f3b5fef88f93a84e3d
RGT1143022 <s1143022@gm.ncue.edu.tw>
2024-12-14 00:12:30 +0800

    Update 6_🏸Player Search.py

--- pages/6_🏸Player Search.py
+++ pages/6_🏸Player Search.py
@@ -241,7 +241,7 @@ if st.session_state.df is not None:
             
             # 使用線作為 handles 並顯示圖例
             red_line = ax1.axhline(y=0, color='red', linestyle='--')
-            brown_line = ax2.axhline(y=0, color='brown', linestyle='--')
+            brown_line = ax2.axhline(y=105, color='brown', linestyle='--')
             ax1.legend(handles=[red_line], labels=['Points nodata'], fontsize=20, loc='upper left')
             ax2.legend(handles=[brown_line], labels=['Rank nodata'], fontsize=20, loc='upper left')
             st.pyplot(fig)
