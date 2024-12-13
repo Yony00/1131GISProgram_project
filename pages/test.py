a31bb13a5a599831721b18be65de9e229a33c548
RGT1143022 <s1143022@gm.ncue.edu.tw>
2024-12-13 22:02:35 +0800

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -4,11 +4,11 @@ import streamlit as st
 
 # 創建第一個地圖 m1
 m1 = folium.Map(location=[0, 0], zoom_start=5, control_scale=True)  # 使用控制縮放和比例尺
-folium.TileLayer('Stamen Terrain').add_to(m1)  # 使用不同的底圖 'Stamen Terrain'
+folium.TileLayer('Terrain').add_to(m1)  # 使用不同的底圖 'Stamen Terrain'
 
 # 創建第二個地圖 m2
 m2 = folium.Map(location=[0, 0], zoom_start=5, control_scale=True)  # 使用控制縮放和比例尺
-folium.TileLayer('Stamen Toner').add_to(m2)  # 使用不同的底圖 'Stamen Toner'
+folium.TileLayer('Toner').add_to(m2)  # 使用不同的底圖 'Stamen Toner'
 
 # 使用 Streamlit 展示地圖
 row1, row2 = st.columns(2)
