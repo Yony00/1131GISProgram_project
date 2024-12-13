88368427e0183cdfdb45a330646416d7a8dfaafa
RGT1143022 <s1143022@gm.ncue.edu.tw>
2024-12-13 22:20:34 +0800

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -11,3 +11,16 @@ output = st_folium(m, width=700, height=500, key="map1")
 
 st.write(output['center'])
 st.write(output['zoom'])
+
+
+# 創建地圖
+m2 = folium.Map(location=[0, 0], zoom_start=2)
+
+# 在 Streamlit 中顯示地圖
+output2 = st_folium(m2, width=700, height=500, key="map1")
+
+st.write(output2['center'])
+st.write(output2['zoom'])
+
+
+
