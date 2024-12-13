84c6429d99d2e85302b6127d300df7e8946a5079
RGT1143022 <s1143022@gm.ncue.edu.tw>
2024-12-13 22:34:04 +0800

    Update test.py

--- pages/test.py
+++ pages/test.py
@@ -9,18 +9,13 @@ m = folium.Map(location=[0, 0], zoom_start=2)
 # 在 Streamlit 中顯示地圖
 output = st_folium(m, width=700, height=500, key="map1")
 
-st.write(output['center'])
-st.write(output['zoom'])
-
-
 # 創建地圖
 m2 = folium.Map(location=[output['center']['lat'],output['center']['lng']], zoom_start=2)
+folium.Marker([120, 24], popup="This is a point").add_to(m2)
 
 # 在 Streamlit 中顯示地圖
 output2 = st_folium(m2, width=700, height=500, key="map2")
 
-st.write(output2['center'])
-st.write(output2['zoom'])
 
 
 
