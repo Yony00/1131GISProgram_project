3b450e1e7cc7bcef89aa0bae42e9af98bdaa32b6
Yony00 <s1143039@gm.ncue.edu.tw>
2024-12-22 21:15:08 +0800

    Update 🍗KFC.py

--- pages/🍗KFC.py
+++ pages/🍗KFC.py
@@ -40,8 +40,8 @@ with col2:
         - **綠色座標點**：{radius} 公尺環域內的 肯德基 分店  
         """
     )
-    markdown = 
-
+    markdown = """
+"""
 st.sidebar.info(markdown)
 logo = "https://raw.githubusercontent.com/Yony00/1131GISProgram_project/refs/heads/main/KFC.jpg"
 st.sidebar.image(logo)
